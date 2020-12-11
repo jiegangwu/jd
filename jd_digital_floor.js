a577ffeaff94e9f855b45a65ac19d4e911d1283f
jk9527 <294443433@qq.com>
2020-12-11 01:25:08 +0000

    Update jd_digital_floor.js

--- jd_digital_floor.js
+++ jd_digital_floor.js
@@ -42,7 +42,7 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://digital-floor.m.jd.com/adf/index/';
 !(async () => {
-  await requireConfig()
+  //await requireConfig()
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
