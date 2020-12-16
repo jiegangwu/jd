ea7dc377d36fc2d0c3a4560e8599ca70df40607d
jk9527 <294443433@qq.com>
2020-12-16 10:11:11 +0000

    Update jd_watch.js

--- jd_watch.js
+++ jd_watch.js
@@ -121,6 +121,7 @@ async function jdHealth() {
     let i = 0, j = $.task.times
     while(j < $.task.maxTimes) {
       if (!acceptBody[i]) break
+      await getTaskList()
       let res = await acceptTask(acceptBody[++i])
       if (res['success']) {
         await $.wait(10000)
