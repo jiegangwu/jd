ba6864c02f74f36e44e812c9a596edc4a4f331b8
kai.jiang <5584529+jk9527@user.noreply.gitee.com>
2020-12-23 11:08:39 +0800

    update jd_small_home.js.

--- jd_small_home.js
+++ jd_small_home.js
@@ -785,7 +785,9 @@ function login(userName) {
 }
 function updateInviteCode(url = 'https://gitee.com/jk9527/updateTeam/raw/my_master/jd_updateSmallHomeInviteCode.json') {
   return new Promise(resolve => {
-    $.get({url}, async (err, resp, data) => {
+   $.get({url, headers:{
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
+      }}, async (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
@@ -802,7 +804,9 @@ function updateInviteCode(url = 'https://gitee.com/jk9527/updateTeam/raw/my_mast
 }
 function updateInviteCodeCDN(url = 'https://gitee.com/jk9527/updateTeam/raw/my_master/jd_updateSmallHomeInviteCode.json') {
   return new Promise(async resolve => {
-    $.get({url}, async (err, resp, data) => {
+    $.get({url, headers:{
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
+      }}, async (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
