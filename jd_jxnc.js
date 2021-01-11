aa4f96dec0d30386039c4c9e38b2b036be7bb0a6
kai.jiang <5584529+jk9527@user.noreply.gitee.com>
2021-01-11 22:17:01 +0800

    update jd_jxnc.js.

--- jd_jxnc.js
+++ jd_jxnc.js
@@ -41,7 +41,7 @@ let currentCookie = ''; // 当前用户 cookie
 let tokenNull = {'farm_jstoken': '', 'phoneid': '', 'timestamp': ''}; // 内置一份空的 token
 let tokenArr = []; // 用户 token 数组
 let currentToken = {}; // 当前用户 token
-const shareCode = '4c34c49e2c37f15da8fba53026e0bc0a@c69eead4e7da881fb46aede0188e30f3@278d3aa9e52ee4444d5979c35a8e9c75'; // 内置助力码
+const shareCode = '14abfdd1e3874210b4e479001e3935d0@0b388f362d734a00b10a21dc2cd7a106@8b02f730d996433297889ce276a196ee@8e1902b8ca434b4ea46e285c149d4fe6'; // 内置助力码
 let jxncShareCodeArr = []; // 用户 助力码 数组
 let currentShareCode = []; // 当前用户 要助力的助力码
 const openUrl = `openjd://virtual?params=${encodeURIComponent('{ "category": "jump", "des": "m", "url": "https://wqsh.jd.com/sns/201912/12/jxnc/detail.html?ptag=7155.9.32&smp=b47f4790d7b2a024e75279f55f6249b9&active=jdnc_1_chelizi1205_2"}',)}`; // 打开京喜农场
@@ -244,20 +244,6 @@ async function jdJXNC() {
             await submitInviteId($.UserName);
             await $.wait(500);
             let next = await helpFriends();
-            if (next) {
-                while (true) {
-                    assistUserShareCode = await getAssistUser();
-                    if (assistUserShareCode) {
-                        await $.wait(300);
-                        next = await helpShareCode(assistUserShareCode);
-                        if (next) {
-                            await $.wait(200);
-                            continue;
-                        }
-                    }
-                    break;
-                }
-            }
         }
     }
     await showMsg()
