23975cfa01140b38ecb7859dfb9f8196fa819399
jk9527 <5584529+jk9527@user.noreply.gitee.com>
2021-01-19 14:31:54 +0800

    update docker/kill_joy.sh.

--- docker/kill_joy.sh
+++ docker/kill_joy.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
   echo "杀掉jd_crazy_joy_coin任务，并重启"
+while true
+do
   pid="$(ps -ef|grep "jd_crazy_joy_coin" | awk '{print $2}'|head -n1)"
   echo $pid
-  kill -9 $pid
+  if[ ! -n $pid ];then
+   kill -9 $pid
+   echo "kill success " $pid
+   continue
+  else
+   break
+  fi
+done
   echo "配置jd_crazy_joy_coin重启完成"
